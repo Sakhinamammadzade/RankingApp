e72c74ed4ace197e76efe8a588b1d228e42520ef
M.Sakina <sakhinamammadzade0406@gmail.com>
2023-01-04 19:10:18 +0400

    React with .Net Web API

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,27 @@
+﻿import React, { useState, useEffect } from 'react';
+
+export default function RankItems(){
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+    console.log(items)
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, []);
+
+    return (
+        <main>
+            {
+                (items != null) ? items.map((item) => <h3>{item.title}</h3>) : <div>Loading....</div>
+            }
+
+        </main>
+    )
+}
+
